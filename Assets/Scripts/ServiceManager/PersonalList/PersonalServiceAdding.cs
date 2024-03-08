74c48bb6ca06797564917563ff3a4e65c3508f5b
SlivaDeda <123898451+CodWindy32@users.noreply.github.com>
2024-03-08 13:47:50 +0300

    FIve commit

---
+++ Assets/Scripts/ServiceManager/PersonalList/PersonalServiceAdding.cs
@@ -0,0 +1,25 @@
+using DatabaseManager;
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace PersonalList
+{
+    public class PersonalServiceAdding : MonoBehaviour
+    {
+        [SerializeField] private TMP_Dropdown _dropdownRole;
+
+        [SerializeField] private TMP_InputField inputName;
+        [SerializeField] private TMP_InputField inputLogin;
+        [SerializeField] private TMP_InputField inputPassword;
+        [SerializeField] private TMP_InputField inputAge;
+
+        public void AddPersonal()
+        {
+            string role = SqlQuery.ExecuteQuerySelect($"SELECT roleId FROM Roles WHERE roleName = '{_dropdownRole.itemText}'", false);
+            SqlQuery.ExecuteQueryEditing($"INSERT INTO Timetables (fullName, age, login, password, roleId) VALUES('{inputName.text}'," +
+                $" '{Convert.ToInt32(inputAge.text)}',{inputLogin.text},{inputPassword.text},'{Convert.ToInt32(role)}')");
+        }
+    }
+}
+
